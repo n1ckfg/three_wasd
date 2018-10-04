56b995fc597a44dded70c588aa26caa930aaae8c
Nick Fox-Gieg <nick@fox-gieg.com>
2018-10-04 08:55:20 -0400

    web test

--- ref/webvr-polyfill/js/main.js
+++ ref/webvr-polyfill/js/main.js
@@ -1,8 +1,8 @@
 function main() {
 
-    //document.addEventListener('touchmove', function(e) {
-      //e.preventDefault();
-    //});
+    document.addEventListener('touchmove', function(e) {
+      e.preventDefault();
+    });
 
     // Get config from URL
     var config = (function() {
@@ -28,12 +28,11 @@ function main() {
 
     var polyfill = new WebVRPolyfill(config);
 
-    console.log("Using webvr-polyfill version " + WebVRPolyfill.version +
-                " with configuration: " + JSON.stringify(config));
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(Math.floor(window.devicePixelRatio));
-
-    // Append the canvas element created by the renderer to document body element.
+    console.log("Using webvr-polyfill version " + WebVRPolyfill.version + " with configuration: " + JSON.stringify(config));
+    
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     var canvas = renderer.domElement;
     document.body.appendChild(canvas);
 
@@ -44,19 +43,22 @@ function main() {
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 
     // Create a reticle
+    /*
     var reticle = new THREE.Mesh(
       new THREE.RingBufferGeometry(0.005, 0.01, 15),
       new THREE.MeshBasicMaterial({ color: 0xffffff })
     );
     reticle.position.z = -0.5;
     camera.add(reticle);
+    */
     scene.add(camera);
 
     // Apply VR stereo rendering to renderer.
-    var effect = new THREE.VREffect(renderer);
-    effect.setSize(canvas.clientWidth, canvas.clientHeight, false);
+    //var effect = new THREE.VREffect(renderer);
+    //effect.setSize(canvas.clientWidth, canvas.clientHeight, false);
 
-    var vrDisplay, controls;
+    //var vrDisplay, 
+    var controls;
 
     // Add a repeating grid as a skybox.
     var boxWidth = 5;
@@ -91,6 +93,7 @@ function main() {
 
     // The polyfill provides this in the event this browser
     // does not support WebVR 1.1
+    /*
     navigator.getVRDisplays().then(function (vrDisplays) {
       // If we have a native display, or we have a CardboardVRDisplay
       // from the polyfill, use it
@@ -106,20 +109,22 @@ function main() {
       // Otherwise, we're on a desktop environment with no native
       // displays, so provide controls for a monoscopic desktop view
       else {
+        */
         controls = new THREE.OrbitControls(camera);
         controls.target.set(0, 0, -1);
 
         // Disable the "Enter VR" button
-        var enterVRButton = document.querySelector('#vr');
-        enterVRButton.disabled = true;
+        //var enterVRButton = document.querySelector('#vr');
+        //enterVRButton.disabled = true;
 
         // Kick off the render loop.
         requestAnimationFrame(animate);
-      }
-    });
+      //}
+    //});
 
     // Request animation frame loop function
     var lastRender = 0;
+    
     function animate(timestamp) {
       var delta = Math.min(timestamp - lastRender, 500);
       lastRender = timestamp;
@@ -131,15 +136,16 @@ function main() {
       controls.update();
 
       // Render the scene.
-      effect.render(scene, camera);
+      //effect.render(scene, camera);
+      renderer.render(scene, camera);
 
       // Keep looping; if using a VRDisplay, call its requestAnimationFrame,
       // otherwise call window.requestAnimationFrame.
-      if (vrDisplay) {
-        vrDisplay.requestAnimationFrame(animate);
-      } else {
+      //if (vrDisplay) {
+        //vrDisplay.requestAnimationFrame(animate);
+      //} else {
         requestAnimationFrame(animate);
-      }
+      //}
     }
 
     function onResize() {
@@ -150,13 +156,14 @@ function main() {
         onResize.resizeDelay = setTimeout(function () {
           onResize.resizeDelay = null;
           console.log('Resizing to %s x %s.', canvas.clientWidth, canvas.clientHeight);
-          effect.setSize(canvas.clientWidth, canvas.clientHeight, false);
+          //effect.setSize(canvas.clientWidth, canvas.clientHeight, false);
           camera.aspect = canvas.clientWidth / canvas.clientHeight;
           camera.updateProjectionMatrix();
         }, 250);
       }
     }
 
+    /*
     function onVRDisplayPresentChange() {
       console.log('onVRDisplayPresentChange');
       onResize();
@@ -166,19 +173,19 @@ function main() {
     function onVRDisplayConnect(e) {
       console.log('onVRDisplayConnect', (e.display || (e.detail && e.detail.display)));
     }
-
+  */
     // Resize the WebGL canvas when we resize and also when we change modes.
     window.addEventListener('resize', onResize);
-    window.addEventListener('vrdisplaypresentchange', onVRDisplayPresentChange);
-    window.addEventListener('vrdisplayconnect', onVRDisplayConnect);
+    //window.addEventListener('vrdisplaypresentchange', onVRDisplayPresentChange);
+    //window.addEventListener('vrdisplayconnect', onVRDisplayConnect);
 
     // Button click handlers.
     document.querySelector('button#fullscreen').addEventListener('click', function() {
       enterFullscreen(renderer.domElement);
     });
-    document.querySelector('button#vr').addEventListener('click', function() {
-      vrDisplay.requestPresent([{source: renderer.domElement}]);
-    });
+    //document.querySelector('button#vr').addEventListener('click', function() {
+      //vrDisplay.requestPresent([{source: renderer.domElement}]);
+    //});
 
     function enterFullscreen (el) {
       if (el.requestFullscreen) {
